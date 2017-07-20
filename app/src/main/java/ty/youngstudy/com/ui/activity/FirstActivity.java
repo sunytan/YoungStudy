8c260d22c4985987578407dc6695ad418afacd94
yang.tan <yang.tan@flyingwings.cn>
2017-07-20 15:58:38 +0800

    学习ButterKnife框架，改用注释的方式初始化资源

--- app/src/main/java/ty/youngstudy/com/ui/activity/FirstActivity.java
+++ app/src/main/java/ty/youngstudy/com/ui/activity/FirstActivity.java
@@ -3,11 +3,11 @@ package ty.youngstudy.com.ui.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,20 +16,51 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
+import butterknife.BindDrawable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ty.youngstudy.com.R;
 import ty.youngstudy.com.ui.activity.base.BaseActivity;
 
-public class FirstActivity extends BaseActivity implements View.OnClickListener{
+public class FirstActivity extends BaseActivity {
 
-    private ViewPager viewPager = null;
-    private ImageView imageView0 = null;
-    private ImageView imageView1 = null;
-    private ImageView imageView2 = null;
-    private ImageView imageView3 = null;
-    private AppCompatButton btn_welcome = null;
+    @BindView(R.id.viewpager_first)
+    ViewPager viewpagerFirst;
+
+    @BindView(R.id.image_first)
+    ImageView imageFirst;
+
+    @BindView(R.id.image_second)
+    ImageView imageSecond;
+
+    @BindView(R.id.image_third)
+    ImageView imageThird;
+
+    @BindView(R.id.image_forth)
+    ImageView imageForth;
+
+    @BindDrawable(R.mipmap.page_now)
+    Drawable page_now;
+
+    @BindDrawable(R.mipmap.page)
+    Drawable page;
+
+    @BindView(R.id.btn_welcome)
+    AppCompatButton btn_welcome;
+
+    @OnClick(R.id.btn_welcome)
+    public void onClick(){
+        Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
     private ArrayList<View> viewList = new ArrayList<View>();
     private ArrayList<String> pagetitle = new ArrayList<String>();
 
@@ -43,68 +74,56 @@ public class FirstActivity extends BaseActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first);
+        //ButterKnife.bind(this);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         /* 初始化化 */
-        initView();
+        /*initView();*/
         initEvent();
         initArray();
         /* 设置适配器 */
-        viewPager.setAdapter(new MyPagerAdapter());
+        viewpagerFirst.setAdapter(new MyPagerAdapter());
         /* 设置应用第一次启动为false */
         setAppStart();
     }
 
     /* 应用已经启动过来，下次不再进入这个界面 */
-    private void setAppStart(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    private void setAppStart() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("first_start",false);
+        editor.putBoolean("first_start", false);
         editor.apply();
     }
 
     /* 初始化视图 */
-    private void initView(){
-        viewPager = (ViewPager) findViewById(R.id.viewpager_first);
+/*    private void initView() {
         imageView0 = (ImageView) findViewById(R.id.image_first);
         imageView1 = (ImageView) findViewById(R.id.image_second);
         imageView2 = (ImageView) findViewById(R.id.image_third);
         imageView3 = (ImageView) findViewById(R.id.image_forth);
         btn_welcome = (AppCompatButton) findViewById(R.id.btn_welcome);
-    }
+    }*/
 
     /* 初始化事件 */
-    private void initEvent(){
-        viewPager.setOnPageChangeListener(new MyOnPageChangeListener());
-        btn_welcome.setOnClickListener(this);
+    private void initEvent() {
+        viewpagerFirst.setOnPageChangeListener(new MyOnPageChangeListener());
+        /*btn_welcome.setOnClickListener(this);*/
     }
 
     /* 初始化数据 */
-    private void initArray(){
-        LayoutInflater li = LayoutInflater.from(this);
-        View view1 = li.inflate(R.layout.first_pager,null);
-        viewList.add(View.inflate(this,R.layout.first_pager,null));
-        viewList.add(View.inflate(this,R.layout.second_pager,null));
-        viewList.add(View.inflate(this,R.layout.third_pager,null));
-        viewList.add(View.inflate(this,R.layout.forth_pager,null));
+    private void initArray() {
+        viewList.add(View.inflate(this, R.layout.first_pager, null));
+        viewList.add(View.inflate(this, R.layout.second_pager, null));
+        viewList.add(View.inflate(this, R.layout.third_pager, null));
+        viewList.add(View.inflate(this, R.layout.forth_pager, null));
         pagetitle.add("first");
         pagetitle.add("second");
         pagetitle.add("third");
         pagetitle.add("forth");
     }
 
-    /* 点击事件处理 */
-    @Override
-    public void onClick(View view) {
-        if (view == btn_welcome) {
-            Intent intent = new Intent(FirstActivity.this,MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-        }
-    }
-
-    private void setButtonVisible(int position){
+    private void setButtonVisible(int position) {
         if (position == 3) {
             btn_welcome.setVisibility(View.VISIBLE);
         } else {
@@ -114,33 +133,33 @@ public class FirstActivity extends BaseActivity implements View.OnClickListener{
     }
 
     /* 页面改变监听器 */
-    private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener{
+    private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
 
         @Override
         public void onPageSelected(int position) {
-            switch (position){
+            switch (position) {
                 case 0:
-                    imageView0.setImageDrawable(getResources().getDrawable(R.mipmap.page_now));
-                    imageView1.setImageDrawable(getResources().getDrawable(R.mipmap.page));
+                    imageFirst.setImageDrawable(page_now);
+                    imageSecond.setImageDrawable(page);
                     break;
                 case 1:
-                    imageView1.setImageDrawable(getResources().getDrawable(R.mipmap.page_now));
-                    imageView0.setImageDrawable(getResources().getDrawable(R.mipmap.page));
-                    imageView2.setImageDrawable(getResources().getDrawable(R.mipmap.page));
+                    imageSecond.setImageDrawable(page_now);
+                    imageFirst.setImageDrawable(page);
+                    imageThird.setImageDrawable(page);
                     break;
                 case 2:
-                    imageView2.setImageDrawable(getResources().getDrawable(R.mipmap.page_now));
-                    imageView1.setImageDrawable(getResources().getDrawable(R.mipmap.page));
-                    imageView3.setImageDrawable(getResources().getDrawable(R.mipmap.page));
+                    imageThird.setImageDrawable(page_now);
+                    imageSecond.setImageDrawable(page);
+                    imageForth.setImageDrawable(page);
                     break;
                 case 3:
-                    imageView3.setImageDrawable(getResources().getDrawable(R.mipmap.page_now));
-                    imageView2.setImageDrawable(getResources().getDrawable(R.mipmap.page));
+                    imageForth.setImageDrawable(page_now);
+                    imageThird.setImageDrawable(page);
                     break;
                 default:
                     break;
             }
-            Log.i("tanyang","position = "+position);
+            Log.i("tanyang", "position = " + position);
             setButtonVisible(position);
         }
 
@@ -156,11 +175,11 @@ public class FirstActivity extends BaseActivity implements View.OnClickListener{
     }
 
     /* 页面适配器 */
-    private class MyPagerAdapter extends PagerAdapter{
+    private class MyPagerAdapter extends PagerAdapter {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            ((ViewPager)container).addView(viewList.get(position));
+            ((ViewPager) container).addView(viewList.get(position));
             return viewList.get(position);
         }
 
@@ -176,7 +195,7 @@ public class FirstActivity extends BaseActivity implements View.OnClickListener{
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            ((ViewPager)container).removeView(viewList.get(position));
+            ((ViewPager) container).removeView(viewList.get(position));
         }
 
         @Override
--- app/src/main/java/ty/youngstudy/com/ui/activity/base/BaseActivity.java
+++ app/src/main/java/ty/youngstudy/com/ui/activity/base/BaseActivity.java
@@ -1,10 +1,12 @@
 package ty.youngstudy.com.ui.activity.base;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
 import ty.youngstudy.com.ui.view.base.BaseView;
 
 /**
@@ -24,6 +26,12 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseView
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);
     }
 
     public abstract boolean getFirstStart();
